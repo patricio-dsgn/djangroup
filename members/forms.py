cc690855124d172af439040aa87b06f80b8ceb19
patricio-dsgn <patricio.ngg@gmail.com>
2022-05-10 20:15:23 -0400

    groups and readme

--- members/forms.py
+++ members/forms.py
@@ -4,6 +4,7 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
+
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=50, required=True)
     last_name = forms.CharField(max_length=50, required=True)
